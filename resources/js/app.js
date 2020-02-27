7a21bef841b34c4b28ddc23f5489f8b05c66b340
phearoth <msphearoth@gmail.com>
2020-02-27 12:53:25 +0700

    Kickstart Laravel Vuejs Project

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+import ExampleComponent from './components/ExampleComponent';
+import router from './routers/router';
+
+Vue.use(VueRouter);
+const app = new Vue({
+    router,
+    components: {
+      ExampleComponent
+    },
+    el: '#app',
+});
