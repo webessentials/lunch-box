a17883d2bdba97f381bd9229df1bc542ec1b897b
phearoth <msphearoth@gmail.com>
2020-02-27 19:14:11 +0700

    [TASK] UI of Dashboard

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="container mt-5">
+      <div class="d-flex justify-content-center">
+        <button class="btn btn-light btn-circle">
+          15
+        </button>
+        <div class="ml-4">
+          <button class="btn btn-success d-block truncate-text">
+            7 Dishes: Fried lobster
+          </button>
+          <button class="btn btn-success d-block mt-4 truncate-text">
+            8 Dishes: Chicken Soup
+          </button>
+        </div>
+      </div>
+      <ul class="list-group mt-5">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Cras justo odio
+          <span class="badge badge-primary badge-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Dapibus ac facilisis in
+          <span class="badge badge-primary badge-pill">2</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Morbi leo risus
+          <span class="badge badge-primary badge-pill">1</span>
+        </li>
+      </ul>
+    </div>
+    <div class="sticky-footer">
+      <button class="btn btn-primary btn-block">Prepare menu</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
