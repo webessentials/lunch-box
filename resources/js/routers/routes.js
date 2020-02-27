d8d496fe127839dd812f9623021c356336c4fdd9
VeasnaOudom <veasnaoudom.cheat@khalibre.com>
2020-02-27 14:36:23 +0700

    HomePage component

--- resources/js/routers/routes.js
+++ resources/js/routers/routes.js
@@ -1,9 +1,11 @@
 import Foo from '../components/Foo'
 import Bar from '../components/Bar'
+import HomePage from '../components/HomePage'
 
 const routes = [
   { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/bar', component: Bar },
+  { path: '/home', component: HomePage }
 ]
 
 export default routes
